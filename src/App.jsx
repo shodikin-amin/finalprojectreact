4ddc61d3f52f106ce650d60e1d912283031fdc86
margiyonoagung <agung.margyono@gmail.com>
2024-04-28 01:10:26 +0700

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import RegisterLayout from './layout/RegisterLayout'
-
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     {/* <RegisterLayout/> */}
-    </>
+<div>App</div>
   )
 }
 
