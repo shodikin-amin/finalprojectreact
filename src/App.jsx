86611ba2ce90aec8baa621478ffcfe2ad5de2131
Rief <ariefsofianh@gmail.com>
2024-04-29 07:16:12 +0800

    Add: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import Nav from "./components/molecules/Nav";
 import Hero from "./components/molecules/Hero";
+import Footer from "./components/molecules/Footer";
 function App() {
   return (
     <>
       <div className="absolute w-screen bg-hijau1">
         <Nav />
       </div>
-      ,
+    
       <div>
         <Hero />
       </div>
+    
+      <div>
+        <Footer />
+      </div>
+    
     </>
   );
 }
