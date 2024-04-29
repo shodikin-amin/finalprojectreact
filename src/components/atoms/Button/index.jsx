1ab723f913e8184ea3aa7518544c4e0a5d2ecf7b
Asterix627 <alwikhafi27@gmail.com>
2024-04-30 01:33:19 +0700

    add=submit on registerFragment

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const IndexButton = (props) => {
   return (
-    <button className="flex w-full justify-center rounded-md bg-hijau1 px-3 py-1.5 text-l font-semibold text-white hover:bg-hijauMd hover:text-hijau1 active:bg-hijau1 active:text-white focus:outline-none focus:ring focus:ring-hijauMd">
+    <button type={props.type} className="flex w-full justify-center rounded-md bg-hijau1 px-3 py-1.5 text-l font-semibold text-white hover:bg-hijauMd hover:text-hijau1 active:bg-hijau1 active:text-white focus:outline-none focus:ring focus:ring-hijauMd">
       {props.name}
     </button>
   );
