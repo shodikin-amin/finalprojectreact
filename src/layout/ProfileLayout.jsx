d2ea0b618d90af54704f3cc2834bf0b75cd648b8
Rief <ariefsofianh@gmail.com>
2024-05-04 02:04:16 +0800

    add: import

--- src/layout/ProfileLayout.jsx
+++ src/layout/ProfileLayout.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import EkskulContainer from '../components/molecules/EkskulContainer'
 import VisiMisiContainer from '../components/molecules/VisiMisiContainer'
+import DaftarGuruContainer from '../components/molecules/DaftarGuruContainer'
+
 
 const ProfileLayout = () => {
   return (
