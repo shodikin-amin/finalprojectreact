028fbc038e6bad3e1a79e5f9c64b51d0bfc7044a
FathimahKhonsa <khonsa.fathimah@gmail.com>
2024-05-03 15:56:43 +0700

    feat: show more & less

--- src/components/atoms/Card/CardEkskul.jsx
+++ src/components/atoms/Card/CardEkskul.jsx
@@ -1,7 +1,9 @@
 import { Card } from 'antd'
-import React from 'react'
+import React, { useState } from 'react'
 
 const CardEkskul = ({item}) => {
+  const [showMore, setShowMore] = useState(false)
+
   return (
     <div className='container p-10'>
       <div className='row justify-content-center'>
@@ -13,7 +15,12 @@ const CardEkskul = ({item}) => {
             <div className='w-3/4 row'>
               <div>
                 <div className='text-lg font-bold'>{val.name}</div>
-                <div className='mt-2'>{val.description}</div>
+                <div className='mt-2 text-justify'>
+                  {showMore? val.description: `${val.description.substring(0, 400)}`}
+                  <button onClick={() => {setShowMore(!showMore)}} className='ms-2 text-blue-600'>
+                    {showMore? "Show less" : "Show more..."}
+                  </button>
+                </div>
               </div>
               <div className='flex justify-between mt-3'>
                 <img src={val.img1} alt='image container' className='h-40 w-52 object-cover object-center rounded-lg'/>
